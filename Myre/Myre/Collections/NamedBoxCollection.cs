fe52a8f761e89ccddf67e480a77c39e4fbe68931
Martin Evans <martindevans@gmail.com>
2011-10-04 16:15:08 +0100

    Added named box collection, a BoxedValueSrtore<string>
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ Myre/Myre/Collections/NamedBoxCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Myre.Collections
+{
+    public class NamedBoxCollection
+        :BoxedValueStore<string>, INamedDataProvider
+    {
+        public Box<T> Get<T>(string name)
+        {
+            return base.Get<T>(name);
+        }
+    }
+}
