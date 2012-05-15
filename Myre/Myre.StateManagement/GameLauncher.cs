8f1065925a3bd1a687423f2d5267a4f02fc95f9e
Martin Evans <martindevans@gmail.com>
2012-05-16 00:48:37 +0100

    Reordered GameLauncher to display message before printing black box
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- Myre/Myre.StateManagement/GameLauncher.cs
+++ Myre/Myre.StateManagement/GameLauncher.cs
@@ -29,17 +29,17 @@ namespace Myre.StateManagement
                     using (var g = new ExceptionGame(e))
                         g.Run();
 #else
-                    using (var blackbox = System.IO.File.CreateText("BLACKBOX " + DateTime.Now.Day + " " + DateTime.Now.Month + " " + DateTime.Now.Year + ".txt"))
+                    if (System.Windows.Forms.MessageBox.Show(ExceptionGame.ErrorMessage + "\nDo you wish to view error details?", ExceptionGame.ErrorTitle, System.Windows.Forms.MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                    {
+                        System.Windows.Forms.MessageBox.Show(e.ToString());
+                    }
+
+                    using (var blackbox = System.IO.File.CreateText("HEIST BLACKBOX " + DateTime.Now.Day + " " + DateTime.Now.Month + " " + DateTime.Now.Year + ".txt"))
                     {
                         blackbox.Write(e.ToString());
 
                         if (e.InnerException != null)
                             blackbox.WriteLine(e.InnerException.ToString());
-                    }
-
-                    if (System.Windows.Forms.MessageBox.Show(ExceptionGame.ErrorMessage + "\nDo you wish to view error details?", ExceptionGame.ErrorTitle, System.Windows.Forms.MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
-                    {
-                        System.Windows.Forms.MessageBox.Show(e.ToString());
                     }
 #endif
                 }
