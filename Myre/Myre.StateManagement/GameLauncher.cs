23b7b906d652138ec6693f49e809a489c5d778d4
Martin Evans <martindevans@gmail.com>
2012-05-16 01:24:46 +0100

    Made Gamelauncher (on windows) have a list of custom exception handles instead of a hardcoded message box -> Removes a dependency on System.Windows.Forms -> Allows things like emailing error messages
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- Myre/Myre.StateManagement/GameLauncher.cs
+++ Myre/Myre.StateManagement/GameLauncher.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using Microsoft.Xna.Framework;
 using System.Diagnostics;
+using System.Net.Mail;
 
 namespace Myre.StateManagement
 {
     public static class GameLauncher
     {
+#if WINDOWS
+        static readonly List<Action<Exception>> _exceptionActions = new List<Action<Exception>>();
+        public static void AddExceptionAction(Action<Exception> action)
+        {
+            _exceptionActions.Add(action);
+        }
+#endif
+
         public static void Run<T>() where T : Game, new()
         {
             if (Debugger.IsAttached)
@@ -29,17 +39,9 @@ namespace Myre.StateManagement
                     using (var g = new ExceptionGame(e))
                         g.Run();
 #else
-                    if (System.Windows.Forms.MessageBox.Show(ExceptionGame.ErrorMessage + "\nDo you wish to view error details?", ExceptionGame.ErrorTitle, System.Windows.Forms.MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                    foreach (Action<Exception> t in _exceptionActions)
                     {
-                        System.Windows.Forms.MessageBox.Show(e.ToString());
-                    }
-
-                    using (var blackbox = System.IO.File.CreateText("HEIST BLACKBOX " + DateTime.Now.Day + " " + DateTime.Now.Month + " " + DateTime.Now.Year + ".txt"))
-                    {
-                        blackbox.Write(e.ToString());
-
-                        if (e.InnerException != null)
-                            blackbox.WriteLine(e.InnerException.ToString());
+                        t(e);
                     }
 #endif
                 }
