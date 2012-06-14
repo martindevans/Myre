621b7d728f9bc9337f5edbacbf0c7b9658a293ac
Martin Evans <martindevans@gmail.com>
2012-06-14 01:05:32 +0100

    Added ProcessBehaviour.cs to Myre.Entities.Behaviours
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ Myre/Myre.Entities/Behaviours/ProcessBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Myre.Entities;
+using Myre.Entities.Behaviours;
+using Myre.Entities.Services;
+
+namespace Myre.Entities.Behaviours
+{
+    public abstract class ProcessBehaviour
+        : Behaviour
+    {
+        protected abstract void Update(float elapsedTime);
+
+        public class Manager<B>
+            : BehaviourManager<B>, IProcess
+            where B : ProcessBehaviour
+        {
+            public new IEnumerable<B> Behaviours
+            {
+                get
+                {
+                    return base.Behaviours;
+                }
+            }
+
+            public bool IsComplete
+            {
+                get { return false; }
+            }
+
+            public override void Initialise(Scene scene)
+            {
+                scene.GetService<ProcessService>().Add(this);
+                base.Initialise(scene);
+            }
+
+            void IProcess.Update(float elapsedTime)
+            {
+                foreach (var item in Behaviours)
+                    item.Update(elapsedTime);
+            }
+        }
+    }
+}
