b0dd07c5c8a6c326ae50db67d59e0032638860a3
Martin Evans <martindevans@gmail.com>
2013-01-12 13:05:03 +0000

    Modified EventService so that event listeners are invoked in reverse order of subscription (most recent subscriber first) Cleaned up EventService code formatting to R# standards Cleaned up spinlock a little
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- Myre/Myre.Entities/Events/Event.cs
+++ Myre/Myre.Entities/Events/Event.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Myre.Entities.Events
 {
@@ -11,7 +8,7 @@ namespace Myre.Entities.Events
     /// <typeparam name="T">The type of event data sent.</typeparam>
     /// <param name="data">The event data sent.</param>
     /// <param name="scope">The scope of the transmitted event.</param>
-    public delegate void MyreEventHandler<T>(T data, object scope);
+    public delegate void MyreEventHandler<in T>(T data, object scope);
 
     /// <summary>
     /// A class which represents an event for a specified data type.
@@ -23,20 +20,24 @@ namespace Myre.Entities.Events
         class Invocation
             : IEventInvocation
         {
-            static Queue<Invocation> pool = new Queue<Invocation>();
-            static SpinLock spinLock;
+            static readonly Queue<Invocation> _pool = new Queue<Invocation>();
+            private static SpinLock _spinLock = new SpinLock();
 
             public Data Data;
             public Event<Data> Event;
 
 #if DEBUG
+// ReSharper disable NotAccessedField.Local (Debugging Aid)
             public System.Diagnostics.StackTrace Stack;
+// ReSharper restore NotAccessedField.Local
 #endif
 
             public void Execute()
             {
-                for (int i = 0; i < Event.listeners.Count; i++)
-                    Event.listeners[i].HandleEvent(Data, Event.Scope);
+                //Loop over event listeners backwards so most recent handlers are executed first
+                //This makes events compatible with using them as a chained system where more recent handlers can temporarily block lower handlers (by modifying the event data)
+                for (int i = Event._listeners.Count - 1; i >= 0; i--)
+                    Event._listeners[i].HandleEvent(Data, Event.Scope);
 
                 Event.TriggerEvent(Data);
             }
@@ -46,12 +47,12 @@ namespace Myre.Entities.Events
                 Data = default(Data);
                 try
                 {
-                    spinLock.Lock();
-                    pool.Enqueue(this);
+                    _spinLock.Lock();
+                    _pool.Enqueue(this);
                 }
                 finally
                 {
-                    spinLock.Unlock();
+                    _spinLock.Unlock();
                 }
             }
 
@@ -59,23 +60,23 @@ namespace Myre.Entities.Events
             {
                 try
                 {
-                    spinLock.Lock();
-                    if (pool.Count > 0)
-                        return pool.Dequeue();
+                    _spinLock.Lock();
+                    if (_pool.Count > 0)
+                        return _pool.Dequeue();
                     else
                         return new Invocation();
                 }
                 finally
                 {
-                    spinLock.Unlock();
+                    _spinLock.Unlock();
                 }
             }
         }
 
-        private EventService service;
-        private object scope;
-        private Event<Data> global;
-        private List<IEventListener<Data>> listeners;
+        private readonly EventService _service;
+        private readonly object _scope;
+        private readonly Event<Data> _global;
+        private readonly List<IEventListener<Data>> _listeners;
 
         /// <summary>
         /// Occurs when data is sent along this event instance.
@@ -86,19 +87,19 @@ namespace Myre.Entities.Events
         /// Gets the service.
         /// </summary>
         /// <value>The service.</value>
-        public EventService Service { get { return service; } }
+        public EventService Service { get { return _service; } }
 
         /// <summary>
         /// Gets the scope object for this event.
         /// </summary>
-        public object Scope { get { return scope; } }
+        public object Scope { get { return _scope; } }
 
         internal Event(EventService service, object scope = null, Event<Data> globalScoped = null)
         {
-            this.service = service;
-            this.scope = scope;
-            this.global = globalScoped;
-            this.listeners = new List<IEventListener<Data>>();
+            _service = service;
+            _scope = scope;
+            _global = globalScoped;
+            _listeners = new List<IEventListener<Data>>();
         }
 
         /// <summary>
@@ -107,7 +108,7 @@ namespace Myre.Entities.Events
         /// <param name="listener">The listener.</param>
         public void AddListener(IEventListener<Data> listener)
         {
-            listeners.Add(listener);
+            _listeners.Add(listener);
         }
 
         /// <summary>
@@ -117,7 +118,7 @@ namespace Myre.Entities.Events
         /// <returns></returns>
         public bool RemoveListener(IEventListener<Data> listener)
         {
-            return listeners.Remove(listener);
+            return _listeners.Remove(listener);
         }
 
         /// <summary>
@@ -128,8 +129,8 @@ namespace Myre.Entities.Events
         {
             Send(data, this);
 
-            if (global != null)
-                Send(data, global);
+            if (_global != null)
+                Send(data, _global);
         }
 
         private void Send(Data data, Event<Data> channel)
@@ -141,7 +142,7 @@ namespace Myre.Entities.Events
             invocation.Stack = new System.Diagnostics.StackTrace();
 #endif
 
-            service.Queue(invocation);
+            _service.Queue(invocation);
         }
 
         private void TriggerEvent(Data data)
--- Myre/Myre.Entities/SpinLock.cs
+++ Myre/Myre.Entities/SpinLock.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
 
 namespace Myre.Entities
 {
     struct SpinLock
     {
-        private int locked;
+        private const int FALSE = default(int);
+        private const int TRUE = FALSE + 1;     //Ensure false != true
+
+        private int locked;                     //Locked is automatically initialised to default(int);
 
         public void Lock()
         {
-            while (Interlocked.CompareExchange(ref locked, 1, 0) != 0)
+            while (Interlocked.CompareExchange(ref locked, TRUE, FALSE) != FALSE)
                 Thread.Sleep(0);
         }
 
         public void Unlock()
         {
-            Interlocked.CompareExchange(ref locked, 0, 1);
+            Interlocked.CompareExchange(ref locked, FALSE, TRUE);
         }
     }
 }
