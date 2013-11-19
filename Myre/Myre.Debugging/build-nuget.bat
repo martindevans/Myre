76f0270e468e136e2ad1164b20be6b245be6344b
Martin Evans <martindevans@gmail.com>
2013-11-19 18:32:37 +0000

    Added a nuspec package
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ Myre/Myre.Debugging/build-nuget.bat
@@ -0,0 +1,11 @@
+nuget pack "(x86) Myre.Debugging.csproj" -Prop Configuration=Release -IncludeReferencedProjects
+
+REM This looks for all nupkg files in the directory, regardless of version
+REM There should only be one
+for %%A in (*.nupkg) do (
+    REM Push package
+    nuget push %%A
+    
+    REM Delete package now we've used it
+    DEL %%A
+)
