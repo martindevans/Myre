158496599ab84a28766aac8359bd3496d3302c3d
Martin Evans <martindevans@gmail.com>
2014-01-27 15:28:19 +0000

    Incremented version numbers for new released
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- Myre/Myre/Properties/AssemblyInfo.cs
+++ Myre/Myre/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("MyreTests")]
