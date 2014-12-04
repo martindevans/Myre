dcbb2991c8d9dc6298e1f780f78959feac206b04
Martin Evans <martindevans@gmail.com>
2014-12-04 17:27:42 +0000

    bumped version
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- Myre/Myre/Properties/AssemblyInfo.cs
+++ Myre/Myre/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.0")]
+[assembly: AssemblyVersion("2.4.0")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("MyreTests")]
