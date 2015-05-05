1d0b34a3a97ce3739060eb2933662978ab082be9
Martin Evans <martindevans@gmail.com>
2015-05-05 19:12:23 +0100

    Added passthrough importer and processor
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ Myre/Myre.Graphics.Pipeline/Passthrough/PassthroughImporter.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace Myre.Graphics.Pipeline.Passthrough
+{
+    [ContentImporter(".*", DefaultProcessor = "PassthroughProcessor", CacheImportedData = true, DisplayName = "Passthrough Importer")]
+    public class PassthroughImporter
+        : ByteImporter
+    {
+    }
+}
---
+++ Myre/Myre.Graphics.Pipeline/Passthrough/PassthroughProcessor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using System.Collections.Generic;
+
+namespace Myre.Graphics.Pipeline.Passthrough
+{
+    [ContentProcessor(DisplayName = "Passthrough Processor")]
+    public class PassthroughProcessor
+        : ContentProcessor<KeyValuePair<string, byte[]>, byte[]>
+    {
+        public override byte[] Process(KeyValuePair<string, byte[]> input, ContentProcessorContext context)
+        {
+            return input.Value;
+        }
+    }
+}
