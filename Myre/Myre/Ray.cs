734b67b69f3a3e0a988c12f7633651d659eeb85a
Martin Evans <martindevans@gmail.com>
2015-08-25 19:13:04 +0100

    Added additional intersects methods to Ray
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- Myre/Myre/Ray.cs
+++ Myre/Myre/Ray.cs
@@ -1,4 +1,5 @@
-﻿using Myre.Extensions;
+﻿using Microsoft.Xna.Framework.Audio;
+using Myre.Extensions;
 using System.Numerics;
 
 namespace Myre
@@ -20,6 +21,30 @@ namespace Myre
         public Vector3 Direction
         {
             get { return XnaRay.Direction.FromXNA(); }
+        }
+
+        public float? Intersects(BoundingSphere sphere)
+        {
+            // ReSharper disable once ImpureMethodCallOnReadonlyValueField
+            return XnaRay.Intersects(sphere.XnaSphere);
+        }
+
+        public float? Intersects(BoundingBox box)
+        {
+            // ReSharper disable once ImpureMethodCallOnReadonlyValueField
+            return XnaRay.Intersects(box.XnaBox);
+        }
+
+        public float? Intersects(BoundingFrustum frustum)
+        {
+            // ReSharper disable once ImpureMethodCallOnReadonlyValueField
+            return XnaRay.Intersects(frustum.XnaFrustum);
+        }
+
+        public float? Intersects(Plane plane)
+        {
+            // ReSharper disable once ImpureMethodCallOnReadonlyValueField
+            return XnaRay.Intersects(new Microsoft.Xna.Framework.Plane(plane.Normal.ToXNA(), plane.D));
         }
     }
 }
